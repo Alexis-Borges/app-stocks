ddc9eecac41f3a665414c5b780b75d6a34e5c8a5
geoffreyrig4l <grigal@oui.sncf>
2023-05-15 15:49:15 +0200

    bouton pour ajuster le nombre d'articles

--- app/components/Body.jsx
+++ app/components/Body.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { getAllProducts } from "../api/api";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 
 const Body = () => {
   const [products, setProducts] = useState([
@@ -13,14 +14,31 @@ const Body = () => {
   }, []);*/
 
   return (
-    <div className="bg-white  mb-24">
+    <div className="mb-24">
       <h3>All products</h3>
-      <div className="p-12">
+      <div className="p-12 flex flex-wrap bg-[#191D24] rounded-xl">
         {products.map((element, index) => (
-          <section className="bg-red-200" key={index}>
-            <p className="p-12">{element.name}</p>
-            <p>{element.amout}</p>
-          </section>
+          <div
+            className="flex-col items-center p-2 bg-red-200 rounded-xl shadow-sm justify-between h-[300px] fit-content"
+            key={index}
+          >
+            <p className="text-center">{element.name}</p>
+            <p>Quantité : {element.amout}</p>
+            <div className="flex items-center">
+              <AiFillMinusCircle className="text-xl" />
+              <input
+                type="number"
+                className="rounded-xl focus:outline-none p-2 w-20 text-center mx-2"
+              ></input>
+              <AiFillPlusCircle className="text-xl" />
+            </div>
+            <button
+              type="button"
+              className="text-[#8EBCFE] rounded-xl p-2 hover:bg-black"
+            >
+              Modify amount
+            </button>
+          </div>
         ))}
       </div>
     </div>
