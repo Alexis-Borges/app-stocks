84e1e57f8aba2d604531e980a4817181fc0fff29
Alexis-Borges <55685344+Alexis-Borges@users.noreply.github.com>
2023-05-15 14:52:00 +0200

    footer done

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Footer from "app/components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Footer />
     </main>
   );
